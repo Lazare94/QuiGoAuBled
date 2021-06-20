60e693a6d04dfe6b581a9408901e891e4ce620d0
Tchappi <lazare.tchappi@yahoo.fr>
2021-06-20 17:28:27 -0400

    radiobuton

--- Src/screens/Register.js
+++ Src/screens/Register.js
@@ -4,34 +4,56 @@ import { StyleSheet,ScrollView } from 'react-native';
 import {Heading} from '../Composants/Heading';
 import {Input} from  '../Composants/Input';
 import {FilledButton} from  '../Composants/FilledButton';
-import {TextButton} from  '../Composants/TextButton';
 import {Error} from '../Composants/Error';
 import {IconButton} from '../Composants/IconButton';
 import { AuthContainer } from '../Composants/AuthContainer';
+import { RadioButtons } from '../Composants/RadioButton';
+import { useState} from "react";
+
 export function RegisterScreen({navigation}) {
+   const [valeur, setValeur] = useState("123"); 
+   const [value, setValue] = useState(0);
+    const myinput = value === 0 ? (
+        <Input
+            style={styles.input}
+            placeholder={"Email"}
+            keyboardType={"email-address"}
+        />
+    ) : (
+          
+        <Input
+             style={styles.input}
+             placeholder={"Téléphone"}
+            keyboardType={"phone-pad"}
+         />
+    )
+
+    const handleClick = (value) => {
+        setValue(value)
+
+     }
+
   return (
     <AuthContainer> 
       <Heading style={styles.titre}>Inscription</Heading>
+      <RadioButtons
+        Valeur={[{ label: 'Créer compte en utilisant le mail.', value: 0 }, { label: 'Créer compte en utilisant le numéro de téléphone', value: 1 }]}
+        onPress ={handleClick}
+    />
 
       <IconButton style={styles.closeIcon} name={'close'} onPress={()=>{
-
+     
       navigation.pop();
       }} />
 
-      <Error error= {''}/>
+      <Error error= {""}/>
 
       <Input
        style={styles.input} placeholder ={'Nom'}/>
       <Input
        style={styles.input} placeholder ={'Prénom'}/>
-      <Input
-       style={styles.input} placeholder ={'Téléphone'} />
-      <Input 
-      
-      style={styles.input} 
-      placeholder ={'Email'}
-       keyboardType={'email-address'} />
-
+       
+         {myinput}
       <Input
        style={styles.input} placeholder ={'Mot de passe'} secureTextEntry/>
        <FilledButton 
